0603bbbf8e871787f765de711074bf68dcc323e8
NNFNSG\ADMIN <dainam.vdn@gmail.com>
2023-09-09 08:14:47 +0700

    new12

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,4 +1,4 @@
-const API_ROOT = process.env.REACT_APP_API_ENDPOINT || "https://kurumiwebui.vercel.app/"
+const API_ROOT = process.env.REACT_APP_API_ENDPOINT || "https://namuuu-nnyk.onrender.com/"
 
 export {
   API_ROOT
